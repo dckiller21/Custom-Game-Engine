3b376b08b51ffb5b3105380035c5abbc9d1d91be
Zachary Gurtner <zacharygurtner@yahoo.com.au>
2018-07-24 18:27:50 +1000

    Setting Up Engine and linking GLFW

---
+++ Custom Game Engine/Project1/main.cpp
@@ -0,0 +1,14 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+
+int main()
+{
+	if (!glfwInit())
+		std::cout << "Error" << std::endl;
+	else
+		std::cout << "Success" << std::endl;
+
+	return 0; 
+
+}
